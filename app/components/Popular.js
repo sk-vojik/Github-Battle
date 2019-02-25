442698e23caac0b558044210b576d3535f2ca805
Scott Vojik <scott.vojik@gmail.com>
2019-02-24 16:51:10 -0800

    popular component created

---
+++ app/components/Popular.js
@@ -0,0 +1,32 @@
+const React = require('react');
+
+class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All',
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState({ selectedLanguage: lang })
+  }
+
+
+
+  render() {
+    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+    return (
+      <ul className="languages">
+        {languages.map(lang => (
+          <li style={lang === this.state.selectedLanguage ? { color: 'red' } : null } onClick={this.updateLanguage.bind(null, lang)} key={lang}>{lang}</li>
+        ))}
+      </ul>
+    )
+  }
+}
+
+export default Popular;
